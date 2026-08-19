16d805feb8fff491173b33ffa308bad909551012
Abhishek0211005 <ag0245689@gmail.com>
2026-08-20 00:57:15 +0530

    Add files via upload

---
+++ 02_dist.py
@@ -0,0 +1,22 @@
+dist = {
+    "Abhishek":100,
+    "satyam":95,
+    "anurag":50,
+    0:"kolhi"
+}
+print(dist, type(dist))
+# print(dist["satyam"])
+# a = dist["Abhishek"]
+# print(a)
+# print(dist.items())
+# print(dist.values())
+# print(dist.keys())
+# dist.update({"Abhishek":99, 10:"kolhi"})
+# print(dist)
+# print(dist.get("rahul"))
+# print(dist.get("Abhishek"))
+# print(dist["Abhishek2"])
+print(dist.popitem())
+print(dist)
+print(dist.pop("Abhishek"))
+print(dist)
