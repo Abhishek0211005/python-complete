e6da5fbd32419e562eec654cbbdba4eb0e02ef3a
Abhishek0211005 <ag0245689@gmail.com>
2026-08-19 13:13:40 +0530

    Add files via upload

---
+++ 01_dist.py
@@ -0,0 +1,19 @@
+# dist = {
+#     "Abhishek":100,
+#     "satyam":95,
+#     "anurag":50,
+#     0:"kolhi"
+# }
+# print(dist["anurag"])
+
+
+d = {} # Empty dictionary
+marks = {
+    "Harry": 100,
+    "Shubham": 56,
+    "Rohan": 23
+}
+
+
+print(marks, type(marks))
+print(marks["Harry"])
