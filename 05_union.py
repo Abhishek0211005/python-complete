0686fba6817e25687cf33a7f27bc3388ab257414
Abhishek0211005 <ag0245689@gmail.com>
2026-08-23 00:35:05 +0530

    Add files via upload

---
+++ 05_union.py
@@ -0,0 +1,11 @@
+top1 = {20,20,30,40,1}
+top2 = {50,30,60,70}
+print(top1.union(top2))
+print(top1.intersection(top2))
+print(top1.symmetric_difference(top2))
+# a = "false" 
+# print(type(a))
+# a = input("Enter the number:")
+# print(a)
+
+
