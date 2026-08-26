4ee6774806f0b3559cffb466b42269f346f1af2d
Abhishek0211005 <ag0245689@gmail.com>
2026-08-26 07:39:23 +0530

    Add files via upload

---
+++ 01_problem.py
@@ -0,0 +1,8 @@
+words = {
+       "madad": "help",
+       "kutta": "dog",
+       "kursi": "chair",
+       "tu hai chutiya": "you are a phool"
+}
+translate = input("Enter the hindi to english convert word: ")
+print(words[translate])
