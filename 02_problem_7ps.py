20deddb22706305f35aebf0e22a0447fc6dbd33b
Abhishek0211005 <ag0245689@gmail.com>
2026-09-07 10:56:32 +0530

    Add files via upload

---
+++ 02_problem_7ps.py
@@ -0,0 +1,10 @@
+l = ["Harry", "Soham", "Sachin", "Rahul"]
+for name in l:
+    if(name.startswith("H") or name.startswith("R")):
+        print(f"hello {name}")
+
+
+a = ["Abhishek", "Shivam", "Sachin"]
+for i in a:
+    if(i.startswith("S")):
+        print(f"hello {i}")
