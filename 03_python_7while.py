5c901f093aef79a8bbe55dfa04cf02cf9a9c3107
Abhishek0211005 <ag0245689@gmail.com>
2026-09-08 10:48:24 +0530

    Add files via upload

---
+++ 03_python_7while.py
@@ -0,0 +1,12 @@
+# l = [1, "Abhishek", "Harry", "False", "Rahul", "Soniya", "True"]
+# i = 0 
+# while(i<len(l)):
+#     print(l[i])
+#     i = i + 1
+
+l = [1, "Harry", False, "This", "Rohan", "Shubham", "Shubhi"]
+
+for item in l:
+    print(item)
+
+    
