34860b889c8046a9ec4f335ad366a10e1f97ffc1
Abhishek0211005 <ag0245689@gmail.com>
2026-09-15 18:09:02 +0530

    Add files via upload

---
+++ 01_class.py
@@ -0,0 +1,25 @@
+# class Employee:
+#     name = "Abhishek"
+#     language = "Py"
+#     salary = 1200000
+
+# Abhishek = Employee()
+# print(Abhishek.name, Abhishek.language)
+
+# class Employee: 
+#     language = "java" 
+#     salary = 1200000
+
+# Abhishek = Employee()
+# Abhishek.name = "Abhishek"
+# print(Abhishek.name, Abhishek.language, Abhishek.salary)
+
+# ravi = Employee()
+# ravi.name = "ravi chutiya"
+# print(ravi.name, Abhishek.language, Abhishek.salary)
+
+# a = [1,2,3]
+# b = a
+# a +=[4,5]
+# print(b)
+
